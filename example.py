1c251c84ed838e554d7bf835ecb2814ee53326c6
radbatr <radbatr@gmail.com>
2021-12-25 19:28:59 +0500

    Add example.py

---
+++ example.py
@@ -0,0 +1,11 @@
+def get_vat(payment, percent=20):
+	try:
+		peyment = float(payment)
+		vat = payment * percent / 100
+		return "Sum VAT: {}".format(vat)
+	except TypeError:
+		return "Can't count. Check data!"	
+
+result =get_vat(400, 15)
+
+print(result)
